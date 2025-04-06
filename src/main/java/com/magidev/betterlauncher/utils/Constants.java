894a755bf339a78a26eaaf09a539232b56825a8e
MagiDev <catplayinggenshinimpact@gmail.com>
2025-04-06 12:56:33 +0200

    Update Constants.java

--- src/main/java/com/magidev/betterlauncher/utils/Constants.java
+++ src/main/java/com/magidev/betterlauncher/utils/Constants.java
@@ -3,5 +3,5 @@ package com.magidev.betterlauncher.utils;
 public class Constants
 {
     public static final String LAUNCHER_NAME = "Better";
-    public static final double LAUNCHER_VERSION = 1.7;
+    public static final double LAUNCHER_VERSION = 1.8;
 }
